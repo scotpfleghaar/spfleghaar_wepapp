c284367dc983925ba2f0b9bfaa456195472ef1bf
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-11-25 08:38:19 -0600

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace spfleghaar_wepapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
